920bca36cb4477215bb3d177565e3b63222b4476
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-12 16:57:38 -0700

    Ran the express.js project generator

--- app.js
+++ app.js
@@ -1,76 +1,60 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.config(['$stateProvider', '$urlRouterProvider',
-  function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: "/home",
-        templateUrl: "/home.html",
-        controller: "MainCtrl"
-      })
-      .state('posts', {
-        url: "/posts/{id}",
-        templateUrl: "/posts.html",
-        controller: "PostsCtrl"
-      });
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-    $urlRouterProvider.otherwise("home");
-  }
-]);
+var app = express();
 
-app.factory('posts', [function () {
-  return {
-    posts: [
-      { title: "post 1", upvotes: 0, comments: [] }
-    ]
-  }
-}]);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
-  function ($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id];
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-    $scope.addComment = function () {
-      if ($scope.body === "") {
-        return;
-      }
+app.use('/', routes);
+app.use('/users', users);
 
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    };
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-    $scope.incrementUpvotes = function (comment) {
-      comment.upvotes += 1;
-    }
-  }]
-);
+// error handlers
 
-app.controller('MainCtrl', ['$scope', 'posts',
-  function ($scope, posts) {
-    $scope.posts = posts.posts;
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-    $scope.addPost = function () {
-      if (!$scope.title || $scope.title === '') {
-        return;
-      }
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-      $scope.posts.push({ title: $scope.title, upvotes: 0, link: $scope.link,
-        comments: [
-          { author: 'Joe', body: 'Cool post!', upvotes: 0 },
-          { author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0 }
-        ]
-      });
 
-      $scope.title = '';
-      $scope.link = '';
-    };
-
-    $scope.incrementUpvotes = function (post) {
-      post.upvotes += 1;
-    }
-  }]
-);
+module.exports = app;
