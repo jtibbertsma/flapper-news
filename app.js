87283485ac107ac444929d8037d2223a2dc76b26
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-12 16:50:14 -0700

    Finished angular section

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.config(['$stateProvider', '$urlRouterProvider',
         url: "/home",
         templateUrl: "/home.html",
         controller: "MainCtrl"
+      })
+      .state('posts', {
+        url: "/posts/{id}",
+        templateUrl: "/posts.html",
+        controller: "PostsCtrl"
       });
 
     $urlRouterProvider.otherwise("home");
@@ -15,9 +20,34 @@ app.config(['$stateProvider', '$urlRouterProvider',
 
 app.factory('posts', [function () {
   return {
-    posts: []
+    posts: [
+      { title: "post 1", upvotes: 0, comments: [] }
+    ]
   }
 }]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
+  function ($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function () {
+      if ($scope.body === "") {
+        return;
+      }
+
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+
+    $scope.incrementUpvotes = function (comment) {
+      comment.upvotes += 1;
+    }
+  }]
+);
 
 app.controller('MainCtrl', ['$scope', 'posts',
   function ($scope, posts) {
@@ -28,7 +58,13 @@ app.controller('MainCtrl', ['$scope', 'posts',
         return;
       }
 
-      $scope.posts.push({ title: $scope.title, upvotes: 0, link: $scope.link });
+      $scope.posts.push({ title: $scope.title, upvotes: 0, link: $scope.link,
+        comments: [
+          { author: 'Joe', body: 'Cool post!', upvotes: 0 },
+          { author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0 }
+        ]
+      });
+
       $scope.title = '';
       $scope.link = '';
     };
