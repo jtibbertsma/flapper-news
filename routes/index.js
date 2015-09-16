56faa53403283d22512b0991c9d52931680dbe64
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-09-16 12:18:01 -0700

    Finished REST routes for posts and comments

--- routes/index.js
+++ routes/index.js
@@ -24,33 +24,66 @@ router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function (err, post) {
-    if (err) {
-      return next(err);
-    }
+    if (err) {  return next(err); }
 
     res.json(post);
   });
 });
 
-router.param('post', function(req, res, next, id) {
+router.param('post', function (req, res, next, id) {
   var query = Post.findById(id);
 
   query.exec(function (err, post) {
-    if (err) {
-      return next(err);
-    }
-
-    if (!post) {
-      return next(new Error('can\'t find post'));
-    }
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
 
     req.post = post;
     return next();
   });
 });
 
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function (err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/upvote', function (req, res, next) {
+  req.post.upvote(function (err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment) {
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
 });
 
 module.exports = router;
